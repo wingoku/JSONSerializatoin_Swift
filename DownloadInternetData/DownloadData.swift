d1df21cf43c8ee4923d4002d394f3131a4c4a23e
Umer Farooq <umerfarooq@Umers-Mac.local>
2014-06-08 06:22:26 -0700

    JSONSerialization v1.0

---
+++ DownloadInternetData/DownloadData.swift
@@ -0,0 +1,130 @@
+//
+//  DownloadData.swift
+//  DownloadInternetData
+//
+//  Created by Umer Farooq on 6/8/14.
+//  Copyright (c) 2014 Umer Farooq. All rights reserved.
+//
+
+import UIKit
+
+
+protocol DownloadDataInterface
+{
+    func sendData(NSDictionary);
+}
+
+class DownloadData: NSObject, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
+   
+    var ddDelegate : DownloadDataInterface?;
+    
+    var nsMutData : NSData = NSData();
+    
+    
+    
+    init(delegate : DownloadDataInterface?)
+    {
+        ddDelegate = delegate;
+        
+        if delegate != nil
+        {
+            println("delegate not nil\n");
+        }
+        
+        if ddDelegate != nil
+        {
+            println("ddSelegate not nil\n");
+        }
+        
+        //var dict : NSDictionary = NSDictionary()
+        
+        //ddDelegate?.sendData(dict)
+        
+        println("init() download data\n");
+    }
+    
+    
+    
+    func startDownload()
+    {
+        var link : String = "http://www.wingoku.bugs3.com/nearestHosps.php?lat=71&long=31&userSelection=dental";
+        
+        var encodedLink : String = link.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding);
+        
+        var nsUrl : NSURL = NSURL(string: encodedLink);
+        
+        var requestedLink : NSURLRequest = NSURLRequest(URL: nsUrl);
+        
+        var connection : NSURLConnection = NSURLConnection(request: requestedLink, delegate: self, startImmediately: false);
+        
+        connection.start();
+        
+        
+        println("Download started\n");
+    }
+    
+    
+    // nSURLConnectionDelegate method
+    func connection(connection: NSURLConnection!, didFailWithError error: NSError!)
+    {
+        println("Error in Connection\n");
+    }
+    
+    
+    // NSURLConnectionDataDelegate method
+    func connection(connection: NSURLConnection!, didReceiveResponse response: NSURLResponse!)
+    {
+        println("Response recieved\n");
+        
+        nsMutData = NSData();
+        
+    }
+
+     // NSURLConnectionDataDelegate method
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!)
+    {
+        println("Got Data\n");
+        
+        if nsMutData != nil
+        {
+            nsMutData = data as NSMutableData;
+        }
+        
+
+        
+    }
+    
+    // NSURLConnectionDataDelegate method
+    func connectionDidFinishLoading(connection: NSURLConnection!)
+    {
+        println("Connection Finished Loading\n");
+     
+        completed();
+    }
+    
+    
+    func completed()
+    {
+        if !self.ddDelegate
+        {
+            println("Delegate is null\n");
+        }
+        
+        if nsMutData != nil
+        {
+            var err : NSError?
+            println("ns data no nill\n");
+            // serializing json
+            
+            
+            // causing EXC_BAD_INSTRUCTION cod= EXC_1386_INVOP
+            let sJson : NSDictionary = NSJSONSerialization.JSONObjectWithData(nsMutData, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+            
+           // var dict: NSDictionary = NSDictionary();
+            
+            ddDelegate?.sendData(sJson)
+        }
+
+    }
+    
+}
---
+++ DownloadInternetData/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  DownloadInternetData
+//
+//  Created by Umer Farooq on 6/8/14.
+//  Copyright (c) 2014 Umer Farooq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, DownloadDataInterface {
+    
+    var dData : DownloadData?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+          dData = DownloadData(delegate: self);
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func sendData(serializedJson: NSDictionary)
+    {
+        println("Interface function VIEW_CONTROLLER\n");
+    }
+    
+    
+    @IBAction func buttonClicked()
+    {
+        println("Button clicked\n");
+        
+        
+        dData?.startDownload();
+    }
+}
+
